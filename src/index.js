1712f1e161f4ed9d2dcdcbfaf96809610663a958
Gavin <gavinvm@protonmail.com>
2024-04-25 16:46:47 +0200

    Set up eslint

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const name = 'James';
+
+const person = { first: name };
+
+console.log(person);
+
+const sayHelloLinting = (fName) => {
+  console.log(`Hello linting, ${fName}`);
+};
+
+const otherName = 'James';
+
+const str = `His name is ${otherName}`;
+console.log(str);
